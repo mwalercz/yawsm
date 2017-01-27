8f037dbca87c858f355ecf5ffa1e67fae60f3644
mwalercz <mwalerczuk@gmail.com>
2017-01-27 01:21:59 +0100

    Remove unnecessary tests in validator

--- ws_dist_queue/master/components/validator.py
+++ ws_dist_queue/master/components/validator.py
@@ -7,27 +7,3 @@ def validate(schema):
             await func(self, req)
         return func_wrapper
     return validate_decorator
-
-
-
-
-if __name__ == '__main__':
-    class TestClass:
-        @validate(schema=NewWorkSchema)
-        def func(self, req):
-            print(req)
-
-
-    req = Request(
-        message={
-            'body': {
-                'command': 'ls',
-                'cwd': 'haha'
-            }
-        },
-        sender=None
-    )
-    clz = TestClass()
-    f = getattr(clz, 'func')
-    f(req)
-    print(req.validated)
